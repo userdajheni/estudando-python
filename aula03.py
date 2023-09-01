afd539a276b41f963020043d8333c75e542b2936
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-09-01 10:47:16 -0300

    tipos de dados

---
+++ aula03.py
@@ -0,0 +1,10 @@
+num_1 = int(input('Digite um numero: '))
+num_2 = int(input('Digite outro numero: '))
+soma = num_1 + num_2
+
+print('A soma entre {} e {} é igual a {}'.format(num_1, num_2, soma))
+
+# int 7, -4, 0, 9875
+# float 4.5, 0.076, -15.223
+# bool True e False
+# str string
